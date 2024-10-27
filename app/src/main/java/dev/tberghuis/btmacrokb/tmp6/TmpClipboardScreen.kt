b23175c2d3c6857bafe1a7c1dc674b1927861e37
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-10-27 13:08:34 +1100

    .

--- app/src/main/java/dev/tberghuis/btmacrokb/tmp6/TmpClipboardScreen.kt
+++ app/src/main/java/dev/tberghuis/btmacrokb/tmp6/TmpClipboardScreen.kt
@@ -4,12 +4,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.tberghuis.btmacrokb.ui.theme.BtMacroKbTheme
 
 @Composable
-fun TmpClipboardScreen() {
+fun TmpClipboardScreen(
+  vm: TmpClipboardVm = viewModel()
+) {
   Column {
     Text("hello clipboard screen")
+    Text(vm.willitblend)
   }
 }
 
---
+++ app/src/main/java/dev/tberghuis/btmacrokb/tmp6/TmpClipboardVm.kt
@@ -0,0 +1,10 @@
+package dev.tberghuis.btmacrokb.tmp6
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class TmpClipboardVm(
+  private val application: Application,
+) : AndroidViewModel(application) {
+  val willitblend = "will it blend"
+}
