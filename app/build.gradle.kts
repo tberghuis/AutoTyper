d08b2993db887c6fc2c0b1b7963dbdab4ab33d93
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-11-06 07:08:28 +1100

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     // todo test sdk 28 29
     minSdk = 28
     targetSdk = 34
-    versionCode = 2
-    versionName = "1.1"
+    versionCode = 3
+    versionName = "1.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables {
