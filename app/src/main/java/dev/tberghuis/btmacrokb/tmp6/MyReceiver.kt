4c083d4be176c35c6cc81504c7828632e2a18d6d
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-11 15:27:10 +1100

    .

--- app/src/main/java/dev/tberghuis/btmacrokb/tmp6/MyReceiver.kt
+++
@@ -1,8 +0,0 @@
-package dev.tberghuis.btmacrokb.tmp6
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class MyReceiver : GlanceAppWidgetReceiver() {
-  override val glanceAppWidget: GlanceAppWidget = MyAppWidget()
-}
