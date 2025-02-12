8914805c00e6fd833be4b68f64f0502514c69be6
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-12 12:10:09 +1100

    .

--- app/src/main/java/dev/tberghuis/btmacrokb/tmp4/Tmp4BtController.kt
+++ app/src/main/java/dev/tberghuis/btmacrokb/tmp4/Tmp4BtController.kt
@@ -4,12 +4,16 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothHidDevice
 import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
 import android.content.Context
 import dev.tberghuis.btmacrokb.util.logd
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class Tmp4BtController(
-  application: Application
+  private val application: Application
 ) {
   private val btAdapter: BluetoothAdapter by lazy {
     val bluetoothManager =
@@ -27,22 +31,39 @@ class Tmp4BtController(
     pairedDevices = btAdapter.bondedDevices?.toList() ?: listOf()
   }
 
-  fun findB450(){
+  fun findB450() {
     b450Device = pairedDevices?.find {
-      it.address.equals("28:7F:CF:BD:00:B9",true)
+      it.address.equals("28:7F:CF:BD:00:B9", true)
     }
     logd("findB450 $b450Device")
   }
 
-  fun getProfileProxy(){
+  fun getProfileProxy(): StateFlow<BluetoothHidDevice?> {
+    val hidDevice = MutableStateFlow<BluetoothHidDevice?>(null)
+    val serviceListener = object : BluetoothProfile.ServiceListener {
+      override fun onServiceConnected(profile: Int, proxy: BluetoothProfile?) {
+        logd("GetProfileProxy onServiceConnected profile $profile")
+        logd("GetProfileProxy onServiceConnected proxy $proxy")
+        hidDevice.value = proxy as BluetoothHidDevice
+      }
+      override fun onServiceDisconnected(profile: Int) {
+        logd("GetProfileProxy onServiceDisconnected profile $profile")
+        hidDevice.value = null
+      }
+    }
+    btAdapter.getProfileProxy(
+      application,
+      serviceListener,
+      BluetoothProfile.HID_DEVICE
+    )
+    return hidDevice
+  }
+
+  fun registerApp() {
 
   }
 
-  fun registerApp(){
-    
-  }
-
-  fun connectB450(){
+  fun connectB450() {
 
   }
 }
