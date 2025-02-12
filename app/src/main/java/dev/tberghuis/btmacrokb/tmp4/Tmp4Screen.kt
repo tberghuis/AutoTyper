0311df99a9d1780897ce8020928f179251c3b297
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-12 13:05:05 +1100

    .

--- app/src/main/java/dev/tberghuis/btmacrokb/tmp4/Tmp4Screen.kt
+++ app/src/main/java/dev/tberghuis/btmacrokb/tmp4/Tmp4Screen.kt
@@ -1,7 +1,6 @@
 package dev.tberghuis.btmacrokb.tmp4
 
 import android.app.Application
-import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -9,12 +8,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 
-
 var controller: Tmp4BtController? = null
 
 @Composable
 fun Tmp4Screen() {
-//  val activity = LocalActivity.current
   val context = LocalContext.current
   controller = remember {
     Tmp4BtController(context.applicationContext as Application)
