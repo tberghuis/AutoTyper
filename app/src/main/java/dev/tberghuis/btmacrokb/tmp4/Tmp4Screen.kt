1a4164566592ff7e46d014a5bc6e102a48649a4e
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-14 08:29:52 +1100

    .

--- app/src/main/java/dev/tberghuis/btmacrokb/tmp4/Tmp4Screen.kt
+++ app/src/main/java/dev/tberghuis/btmacrokb/tmp4/Tmp4Screen.kt
@@ -6,25 +6,31 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
+import dev.tberghuis.btmacrokb.tmp6.SingleUseBtController2
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
 
-var controller: Tmp4BtController? = null
+var controller: SingleUseBtController2? = null
 
 @Composable
 fun Tmp4Screen() {
   val context = LocalContext.current
   controller = remember {
-    Tmp4BtController(context.applicationContext as Application)
+    SingleUseBtController2(context.applicationContext as Application)
   }
+
+  val scope = rememberCoroutineScope()
 
   Column {
     Text("hello tmp4")
-
     Button(onClick = {
-      controller?.sendHello()
+      scope.launch(IO) {
+        controller?.sendPayload("28:7F:CF:BD:00:B9", "hello\n")
+      }
     }) {
       Text("send hello")
     }
-
   }
 }
