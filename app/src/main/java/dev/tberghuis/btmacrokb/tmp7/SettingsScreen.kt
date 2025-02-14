acf4f0cef0786fc0e2ad1ccb8f7b66c6bb547b3e
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-15 10:23:43 +1100

    .

--- app/src/main/java/dev/tberghuis/btmacrokb/tmp7/SettingsScreen.kt
+++ app/src/main/java/dev/tberghuis/btmacrokb/tmp7/SettingsScreen.kt
@@ -2,14 +2,40 @@ package dev.tberghuis.btmacrokb.tmp7
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import dev.tberghuis.btmacrokb.composables.LocalNavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen() {
-  Scaffold() { padding ->
+  val nav = LocalNavController.current
+
+
+  Scaffold(
+    modifier = Modifier,
+    topBar = {
+      TopAppBar(
+        title = { Text("Settings") },
+        modifier = Modifier,
+        navigationIcon = {
+          IconButton(onClick = {
+            nav.navigateUp()
+          }) {
+            Icon(Icons.AutoMirrored.Filled.ArrowBack, "back")
+          }
+        },
+      )
+    },
+  ) { padding ->
     Column(modifier = Modifier.padding(padding)) {
       Text("hello settings")
     }
