32545365a728fdc47253d1f7ea99fe6fdb30661a
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-14 13:46:03 +1100

    .

--- app/src/main/java/dev/tberghuis/btmacrokb/tmp6/Tmp6Screen.kt
+++ app/src/main/java/dev/tberghuis/btmacrokb/tmp6/Tmp6Screen.kt
@@ -3,12 +3,22 @@ package dev.tberghuis.btmacrokb.tmp6
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun Tmp6Screen(
     vm: Tmp6ScreenVm = viewModel()
 ) {
+
+  // hack to only run once even if configuration change
+  rememberSaveable {
+    vm.runOnce()
+    ""
+  }
+
+
+
   Column {
     Text("hello tmp6")
   }
