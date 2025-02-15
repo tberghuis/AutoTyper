7a916dd36d3fcaa04f2860e444e88709d9930d75
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-15 13:21:05 +1100

    .

---
+++ app/src/main/java/dev/tberghuis/btmacrokb/tmp8/DeepLinkDialog.kt
@@ -0,0 +1,27 @@
+package dev.tberghuis.btmacrokb.tmp8
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun Tmp6Screen(
+) {
+  Column {
+    Text("hello tmp 6")
+  }
+}
+
+@Preview
+@Composable
+fun Tmp6ScreenPreview() {
+  Tmp6Screen()
+}
+
+
+@Composable
+fun DeepLinkDialog() {
+
+}
