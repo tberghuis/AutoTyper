473f6772a412540a2527228604b135d4476afd94
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-15 14:57:33 +1100

    .

--- app/src/main/java/dev/tberghuis/btmacrokb/tmp8/DeepLinkDialog.kt
+++ app/src/main/java/dev/tberghuis/btmacrokb/tmp8/DeepLinkDialog.kt
@@ -2,29 +2,20 @@ package dev.tberghuis.btmacrokb.tmp8
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-
 
 @Composable
 fun Tmp6Screen(
