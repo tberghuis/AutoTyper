9675d9401494ad3a18e3ecaf9a3a362a5f034407
Thomas Berghuis <thomas.berghuis@gmail.com>
2025-02-16 08:15:39 +1100

    .

--- app/src/main/java/dev/tberghuis/btmacrokb/tmp6/Tmp6Screen.kt
+++ app/src/main/java/dev/tberghuis/btmacrokb/tmp6/Tmp6Screen.kt
@@ -1,10 +1,16 @@
 package dev.tberghuis.btmacrokb.tmp6
 
 import androidx.activity.compose.LocalActivity
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
@@ -12,7 +18,6 @@ fun DeepLinkScreen(
   vm: DeepLinkScreenVm = viewModel()
 ) {
   val activity = LocalActivity.current
-
   // hack to only run once even if configuration change
   rememberSaveable {
     activity?.intent?.data?.let {
@@ -21,7 +26,18 @@ fun DeepLinkScreen(
     ""
   }
 
-  Column {
-    Text("hello tmp6")
+  Scaffold() { padding ->
+    Column(
+      modifier = Modifier.padding(padding).fillMaxSize(),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+      Column {
+        Text("deeplink: ")
+        Text("address: ")
+        Text("payload: ")
+        Text("result: ")
+      }
+    }
   }
 }
